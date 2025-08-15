d19f5c50791fedceea52bd70da2d6b8aea5c537a
Nicolas Toconas <ntoconas@lexim.solutions>
2025-08-15 18:58:33 -0300

    Cambio en env

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client'
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'https://tu-app-production.up.railway.app'
+console.log(import.meta.env)
 
 export const socket = io(SERVER_URL, {
 	autoConnect: false,
